895d70089548e230011a4f5e0dc920ddbd7bc26e
Eunji <eeunjiilee@gmail.com>
2023-10-06 17:21:37 +0900

    design: media query

--- src/styles/cart-style.js
+++ src/styles/cart-style.js
@@ -9,6 +9,10 @@ export const itemInfo = css`
   display: flex;
   gap: 16px;
   align-items: center;
+
+  @media (max-width: 480px) {
+    /* gap: 0; */
+  }
 `;
 
 export const img = css`
@@ -16,6 +20,14 @@ export const img = css`
   height: 80px;
   object-fit: cover;
   border-radius: 8px;
+
+  @media (max-width: 480px) {
+    /* display: none; */
+  }
+
+  @media (max-width: 240px) {
+    display: none;
+  }
 `;
 
 export const container = css`
@@ -24,17 +36,27 @@ export const container = css`
   display: flex;
   justify-content: space-between;
   font-size: 14px;
+  @media (max-width: 480px) {
+    flex-direction: column;
+    gap: 4px;
+  }
 `;
 
 export const amountStyle = css`
   background-color: #f4f4f4;
   padding: 12px 16px;
+  @media (max-width: 480px) {
+    /* display: none; */
+  }
 `;
 
 export const itemPriceStyle = css`
   background: transparent;
   padding: 8px;
   font-weight: 600;
+  @media (max-width: 240px) {
+    display: none;
+  }
 `;
 
 export const totalPriceContainer = css`
@@ -45,11 +67,26 @@ export const totalPriceContainer = css`
   & p {
     font-size: 14px;
   }
+
+  @media (max-width: 480px) {
+    flex-direction: column;
+    & p {
+      display: none;
+    }
+  }
+
+  @media (max-width: 240px) {
+    padding: 4px;
+  }
 `;
 
 export const wideBtn = css`
   width: 160px;
   font-weight: 600;
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
 `;
 
 export const orderBadgePosition = css`
@@ -62,9 +99,44 @@ export const cartUl = css`
   padding: 0;
   margin: 0;
   list-style: none;
+  max-height: 50vh;
+  overflow: auto;
+  @media (max-width: 480px) {
+    padding: 8px;
+    max-height: calc(80vh - 160px);
+  }
+  @media (max-height: 560px) {
+    max-height: calc(80vh - 160px);
+  }
 `;
 
 export const cartLi = css`
   padding: 0;
   margin: 0;
+`;
+
+export const closeBtn = css`
+  margin: 8px;
+
+  & button {
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    color: #d4d4d4;
+    font-size: 20px;
+  }
+
+  & button:hover {
+    color: #000;
+  }
+
+  @media (max-width: 480px) {
+    margin: 0;
+
+    & button {
+      padding: 0;
+      margin: 0;
+      line-height: 0px;
+    }
+  }
 `;
--- src/styles/ui-style.js
+++ src/styles/ui-style.js
@@ -56,7 +56,7 @@ export const backdrop = css`
 
 export const modal = css`
   position: fixed;
-  top: 20vh;
+  top: 15vh;
   left: 10%;
   width: 80%;
   background-color: #fff;
@@ -64,7 +64,30 @@ export const modal = css`
   border-radius: 14px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
   z-index: 30;
-  animation: slide-down 300ms ease-out forwards;
+  animation: slide-up 300ms ease-out forwards;
+
+  @media (max-width: 480px) {
+    top: 10vh;
+    left: 5%;
+    width: 90%;
+    height: 80vh;
+  }
+
+  @media (max-height: 560px) {
+    top: 10vh;
+    height: 80vh;
+  }
+
+  @keyframes slide-up {
+    from {
+      opacity: 0;
+      transform: translateY(1rem);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
 `;
 
 export const badge = css`
